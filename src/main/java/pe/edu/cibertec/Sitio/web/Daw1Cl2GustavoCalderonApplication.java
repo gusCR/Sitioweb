063b209fb7c742b2374c7b4480e3b8d0f3081e26
Gustavo Calderón Rodríguez <gus951288@gmail.comm>
2023-08-17 20:59:16 -0500

    Mensaje del commit

---
+++ src/main/java/pe/edu/cibertec/Sitio/web/Daw1Cl2GustavoCalderonApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.Sitio.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Daw1Cl2GustavoCalderonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Daw1Cl2GustavoCalderonApplication.class, args);
+	}
+
+}
